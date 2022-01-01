5bafbd7527e21b772ac9f8e50a7eef46f075ccf6
renjithspace <renjithrajeevvk@gmail.com>
2022-01-01 18:25:47 +0530

    Create logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import logo from '../../assets/logo.png'
+
+export default class Logo extends Component {
+  render () {
+    return <img src={logo} alt='Spacerep' />
+  }
+}
