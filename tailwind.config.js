d49d0ba97e65f7a158c5b1788aab9901a9446dc2
rahulspace <rahultp518@gmail.com>
2022-01-01 19:20:11 +0530

    Extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundColor: {
+        primary: '#3D50FA',
+        dark: {
+          primary: '#191A1F',
+          secondary: '#25262C',
+          tertiary: '#32333A',
+          quaternary: '#464646'
+        }
+      },
+      textColor: {
+        primary: '#f5f5f5',
+        secondary: '#c6c6c6',
+        tertiary: '#999999'
+      },
+      borderColor: {
+        primary: '#373737'
+      }
+    }
   },
   plugins: []
 }
