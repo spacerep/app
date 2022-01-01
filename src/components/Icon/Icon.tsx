081cf45a2a95d5295e08448b47c6f5f02ee4e6a6
renjithspace <renjithrajeevvk@gmail.com>
2022-01-01 20:01:11 +0530

    Create icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,37 @@
+import React, { Component, MouseEvent } from 'react'
+
+type IconSize = 'regular' | 'medium'
+
+type SizeStyle = Record<IconSize, string>
+
+interface IconProps {
+  name: string
+  size: IconSize
+  onClick?: (event: MouseEvent) => void
+}
+
+export default class Icon extends Component<IconProps> {
+  sizeStyle: SizeStyle = {
+    regular: 'text-xl',
+    medium: 'text-2xl'
+  }
+
+  get style () {
+    const size = this.sizeStyle[this.props.size]
+    const cursor = this.props.onClick && 'cursor-pointer'
+    return `ri-${this.props.name}-line text-tertiary ${size} ${cursor}`
+  }
+
+  handleClick (event: MouseEvent) {
+    const { onClick } = this.props
+    if (onClick) onClick(event)
+  }
+
+  render () {
+    return (
+      <i
+        className={this.style}
+        onClick={this.handleClick} />
+    )
+  }
+}
