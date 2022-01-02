74e13a3c244babb13edbf3c75f565ad96d36da7e
renjithspace <renjithrajeevvk@gmail.com>
2022-01-02 21:24:39 +0530

    Topics overflow fix

--- src/components/Sidebar/Sidebar.style.ts
+++ src/components/Sidebar/Sidebar.style.ts
@@ -1,6 +1,6 @@
 export default {
-  sidebar: 'bg-dark-primary p-5 h-screen',
+  sidebar: 'flex flex-col bg-dark-primary p-5 h-screen',
   header: 'flex justify-between items-center',
-  topicAdd: 'my-6',
-  topics: 'my-6'
+  topicAdd: 'flex-none mt-6',
+  topics: 'flex-1 mt-6 overflow-y-scroll'
 }
