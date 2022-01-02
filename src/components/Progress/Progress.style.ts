47b05f89d6140eb7188bf08b9593dfb94578aa44
rahulspace <rahultp518@gmail.com>
2022-01-02 13:21:15 +0530

    Create topic component

--- src/components/Progress/Progress.style.ts
+++ src/components/Progress/Progress.style.ts
@@ -1,4 +1,4 @@
 export default {
-  progress: 'h-1 flex bg-dark-quaternary',
+  progress: 'h-0.5 flex bg-dark-quaternary',
   bar: 'bg-primary transition-width'
 }
---
+++ src/components/Topic/Topic.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Progress from '../Progress/Progress'
+import style from './Topic.style'
+
+interface TopicProps {
+  title: string
+  reps: number
+  nextRepAt: string
+  totalNotes: number
+  completedNotes: number
+  active: boolean
+}
+
+export default class Topic extends Component<TopicProps> {
+  get className () {
+    const active = this.props.active ? 'bg-dark-secondary' : 'bg-dark-primary'
+    return `${style.topic} ${active}`
+  }
+
+  render () {
+    return (
+      <div className={this.className}>
+        <div className={style.details}>
+          <h3 className={style.title}>{this.props.title}</h3>
+          <div className={style.repsDetails}>
+            <h4 className={style.repsCount}>{this.props.reps} reps</h4>
+            <small className={style.nextRep}>Next {this.props.nextRepAt}</small>
+          </div>
+        </div>
+        <Progress
+          total={this.props.totalNotes}
+          completed={this.props.completedNotes} />
+      </div>
+    )
+  }
+}
