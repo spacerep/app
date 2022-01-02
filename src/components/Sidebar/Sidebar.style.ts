9ca03c5eccce236d75c824f78c5a3cd199abf905
renjithspace <renjithrajeevvk@gmail.com>
2022-01-02 21:14:40 +0530

    Sidebar: set max height to screen

--- src/components/Sidebar/Sidebar.style.ts
+++ src/components/Sidebar/Sidebar.style.ts
@@ -1,5 +1,5 @@
 export default {
-  sidebar: 'bg-dark-primary p-5',
+  sidebar: 'bg-dark-primary p-5 h-screen',
   header: 'flex justify-between items-center',
   topicAdd: 'my-6',
   topics: 'my-6'
