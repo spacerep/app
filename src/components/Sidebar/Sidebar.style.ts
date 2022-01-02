bfe235d44817922dc197a4c85214b2a9c88f7d4d
renjithspace <renjithrajeevvk@gmail.com>
2022-01-02 21:37:02 +0530

    Set active topic full width

--- src/components/Sidebar/Sidebar.style.ts
+++ src/components/Sidebar/Sidebar.style.ts
@@ -1,6 +1,6 @@
 export default {
-  sidebar: 'flex flex-col bg-dark-primary p-5 h-screen',
-  header: 'flex justify-between items-center',
-  topicAdd: 'flex-none mt-6',
+  sidebar: 'flex flex-col bg-dark-primary py-5 h-screen',
+  header: 'flex justify-between items-center px-5',
+  topicAdd: 'flex-none mt-6 px-5',
   topics: 'flex-1 mt-6 overflow-y-scroll'
 }
