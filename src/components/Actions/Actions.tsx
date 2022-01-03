865562898c8dc5ee36330f48b7119e2d4da3fd46
renjithspace <renjithrajeevvk@gmail.com>
2022-01-03 19:00:13 +0530

    Actions: refactor

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Icon from '../Icon/Icon'
+import Icon, { IconSize } from '../Icon/Icon'
 import style from './Actions.style'
 
 export type ActionName = 'add' |
@@ -10,56 +10,60 @@ export type ActionName = 'add' |
   'export' |
   'delete' |
   'complete'
-type ActionIcons = Record<ActionName, ActionIcon>
-type ActionsScope = 'topic' | 'note'
-type ActionScopeActions = Record<ActionsScope, ActionName[]>
 
-interface ActionIcon {
-  name: ActionName
-  icon: string
-}
+type ActionIcons = Record<ActionName, string>
+type ScopeName = 'topic' | 'note'
+type ScopeActions = Record<ScopeName, ActionName[]>
+type ScopeIconSizes = Record<ScopeName, IconSize>
 
 interface ActionsProps {
   id: number
-  scope: ActionsScope
+  scope: ScopeName
   onClick: (action: ActionName, id: number) => void
 }
 
 export default class Actions extends Component<ActionsProps> {
-  icons: ActionIcons = {
-    add: { name: 'add', icon: 'add' },
-    view: { name: 'view', icon: 'eye' },
-    hide: { name: 'hide', icon: 'eye-off' },
-    edit: { name: 'edit', icon: 'pencil' },
-    import: { name: 'import', icon: 'download' },
-    export: { name: 'export', icon: 'upload' },
-    delete: { name: 'delete', icon: 'delete-bin-6' },
-    complete: { name: 'complete', icon: 'check' }
+  actionIcons: ActionIcons = {
+    add: 'add',
+    view: 'eye',
+    hide: 'eye-off',
+    edit: 'pencil',
+    import: 'download',
+    export: 'upload',
+    delete: 'delete-bin-6',
+    complete: 'check'
   }
 
-  scopeActions: ActionScopeActions = {
+  scopeActions: ScopeActions = {
     topic: ['add', 'hide', 'edit', 'import', 'export', 'delete'],
     note: ['complete', 'edit', 'delete']
+  }
+
+  scopeIconSizes: ScopeIconSizes = {
+    topic: 'medium',
+    note: 'regular'
+  }
+
+  action (action: ActionName, iconSize: IconSize) {
+    const icon = this.actionIcons[action]
+    const handleClick = () => this.props.onClick(action, this.props.id)
+    return (
+      <div
+      className={style.action}
+      key={action}>
+        <Icon
+          name={icon}
+          size={iconSize}
+          onClick={handleClick} />
+      </div>
+    )
   }
 
   get actions () {
     const { scope } = this.props
     const actions = this.scopeActions[scope]
-    return actions.map(action => {
-      const icon = this.icons[action]
-      const size = (scope === 'topic') ? 'medium' : 'regular'
-      const handleClick = () => this.props.onClick(action, this.props.id)
-      return (
-        <div
-          className={style.action}
-          key={action}>
-          <Icon
-            name={icon.icon}
-            size={size}
-            onClick={handleClick} />
-        </div>
-      )
-    })
+    const iconSize = this.scopeIconSizes[scope]
+    return actions.map(action => this.action(action, iconSize))
   }
 
   render () {
